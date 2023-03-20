901eec94eec26a658d61d8d827f3dca3ae1fca7d
CineOP <57627686+CineOP@users.noreply.github.com>
2023-03-20 18:12:11 +0800

    Layout is ready and responsible

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Button[] numButtons = new Button[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < 10; i++) {
+            try {
+                numButtons[i] = findViewById(R.id.class.getField("butt"+String.valueOf(i)).getInt(null));
+            } catch (NoSuchFieldException ignored) {
+
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 
 
